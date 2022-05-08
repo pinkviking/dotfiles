6ab56a5f614e6eb4c066fabb02c9dcbe5b98e5c5
Frosti Grétarsson <gretarssonur@gmail.com>
2022-05-08 23:46:53 +0000

    awesomewm --init

---
+++ awesome/setupsession.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+touchpad="$(xinput list --name-only | grep -i touchpad)"
+if [ ! -z "$touchpad" ]; then
+    xinput set-prop "$touchpad" "libinput Tapping Enabled" 1
+    xinput set-prop "$touchpad" "libinput Natural Scrolling Enabled" 1
+fi
